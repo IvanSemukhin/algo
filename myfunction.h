d434abde7ba89833b36531bbbd6c047a26e4190e
IvanSemukhin <rubberman88@rambler.ru>
2018-02-16 21:57:30 +0500

    add at method

--- myfunction.h
+++ myfunction.h
@@ -23,7 +23,7 @@ template <class X> void revers_vec(std::vector<X>& vec){
     unsigned start = 0;
     unsigned end = vec.size()-1;
     while(start < end)
-        std::swap(vec[start++], vec[end--]);
+        std::swap(vec.at(start++), vec.at(end--));
 }
 //-----------------------------------------------------------------------------
 
@@ -40,16 +40,16 @@ template <class X> void merge_sort(std::vector<X>& vec, long left, long right){
         long size = right-left+1;
         std::vector<X> tmp(size);
         while(pos1 <= half && pos2 <= right)
-            if(vec[pos1] < vec[pos2])
-                tmp[pos3++] = vec[pos1++];
+            if(vec.at(pos1) < vec.at(pos2))
+                tmp.at(pos3++) = vec.at(pos1++);
             else
-                tmp[pos3++] = vec[pos2++];
+                tmp.at(pos3++) = vec.at(pos2++);
         while(pos1 <= half)
-            tmp[pos3++] = vec[pos1++];
+            tmp.at(pos3++) = vec.at(pos1++);
         while(pos2 <= right)
-            tmp[pos3++] = vec[pos2++];
+            tmp.at(pos3++) = vec.at(pos2++);
         for(pos3 = 0; pos3 < size; pos3++)
-            vec[left+pos3] = tmp[pos3];
+            vec.at(left+pos3) = tmp.at(pos3);
         tmp.clear();
     }
 }
@@ -58,9 +58,9 @@ template <class X> void quick_sort(std::vector<X>& vec, long left, long right){
     if(left < right){
         long sup = left;
         for(long i = left; i < right; i++)
-            if(vec[i] < vec[right])
-                std::swap(vec[i], vec[sup++]);
-        std::swap(vec[sup], vec[right]);
+            if(vec.at(i) < vec.at(right))
+                std::swap(vec.at(i), vec.at(sup++));
+        std::swap(vec.at(sup), vec.at(right));
         quick_sort(vec, left, sup-1);
         quick_sort(vec, sup+1, right);
     }
@@ -69,11 +69,11 @@ template <class X> void quick_sort(std::vector<X>& vec, long left, long right){
 template <class X> void insert_sort(std::vector<X>& vec){
     unsigned len = vec.size();
     for(unsigned i = 1; i < len; i++){
-        X elem = vec[i];
+        X elem = vec.at(i);
         unsigned j = i-1;
-        for(;j >= 0 && vec[j] > elem; j--)
-            vec[j+1] = vec[j];
-        vec[j+1] = elem;
+        for(;j >= 0 && vec.at(j) > elem; j--)
+            vec.at(j+1) = vec.at(j);
+        vec.at(j+1) = elem;
     }
 }
 
@@ -81,8 +81,8 @@ template <class X> void select_sort(std::vector<X>& vec){
     unsigned len = vec.size();
     for(unsigned i = 0; i < len-1; i++)
         for(unsigned j = i+1; j< len; j++)
-            if(vec[i] > vec[j])
-                std::swap(vec[i], vec[j]);
+            if(vec.at(i) > vec.at(j))
+                std::swap(vec.at(i), vec.at(j));
 }
 //-----------------------------------------------------------------------------
 
@@ -93,23 +93,23 @@ template <class X> long bin_search(const std::vector<X>& vec, X elem){
     long right = vec.size()-1;
     while(left <= right){
         long half = (left+right)/2;
-        if(vec[half] == elem)
+        if(vec.at(half) == elem)
             return half;
         else
-            vec[half] > elem ? right=half-1 : left=half+1;
+            vec.at(half) > elem ? right=half-1 : left=half+1;
     }
     return -1;
 }
 
 template <class X> long lin_search(std::vector<X>& vec, X elem){
     long right = vec.size()-1;
-    X last = vec[right];
-    vec[right] = elem;
+    X last = vec.at(right);
+    vec.at(right) = elem;
     long index = 0;
-    while(vec[index] != elem)
+    while(vec.at(index) != elem)
         index++;
-    vec[right] = last;
-    if(index < right || vec[right] == elem)
+    vec.at(right) = last;
+    if(index < right || vec.at(right) == elem)
         return index;
     return -1;
 }
